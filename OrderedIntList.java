2480b82a2a52c667f59aa7196ef893b1520d702e
Eric Kim <46500175+PlayerJunK@users.noreply.github.com>
2019-03-16 21:57:45 -0700

    Add files via upload

---
+++ OrderedIntList.java
@@ -0,0 +1,193 @@
+/**	
+ *	Name: Eric Kim
+ *	ClassID: 438
+ *	Assignment #: 3
+ *	Description: The OrderedIntList class inserts an integer into a
+ *				 list in ascending order. It should not take any
+ *				 duplicate values. If the array is full, then the
+ *				 value is inserted and length is increased by 1/2 of
+ *				 of the length. If deleting elements, and the array is
+ *				 1/2 empty, the length is reduced by 1/2. When the array
+ *				 is to be printed, it is printed in form "number space
+ *				 number space number", or returns an empty string if empty.
+ */
+
+package cse360assign3;
+
+public class OrderedIntList 
+{	
+	private int[] array;
+	private int counter;
+	
+	/**
+	 *	Constructor for OrderedIntList
+	 *	makes an int array of size 10
+	 */
+	
+	public OrderedIntList ()
+	{
+		array = new int[10];
+		counter = 0;
+	}
+	
+	/**
+	 * Overloaded constructor for OrderedIntList
+	 * makes an int array of size "size".
+	 * 
+	 * @param the size of the array.
+	 */
+	
+	public OrderedIntList(int size)
+	{
+		array = new int[size];
+		counter = 0;
+	}
+	
+	/**
+	 * Inserts an integer input into the OrderedIntList 
+	 * array of size 10 in ascending order. It continues
+	 * after the max length value by extending the size 
+	 * of the array by 1/2 of its original length, while
+	 * excluding duplicate values.
+	 * 
+	 * It first checks the position in which to insert
+	 * the value in the array, finding position by
+	 * comparison. If the number is found to be a duplicate
+	 * then no operation will happen.
+	 * 
+	 * @param number An integer input
+	 */
+	
+	public void insert (int number) 
+	{
+		int position = 0;
+		boolean duplicate = false;
+		
+		if (size() == length())
+		{
+			int[] temp = new int[length()];
+			for (int iterator = 0; iterator < size(); iterator ++)
+			{
+				temp[iterator] = array[iterator];
+			}
+			array = new int[length() + (length() / 2)];
+			for (int iterator = 0; iterator < temp.length; iterator++)
+			{
+				array[iterator] = temp[iterator];
+			}
+		}
+		
+		for (int iterator = 0; iterator < counter; iterator++)
+		{
+			if (number > array[iterator]) 
+			{
+				position++;
+			}
+			if (number == array[iterator])
+			{
+				duplicate = true;
+			}
+		}
+		
+		if (duplicate == false)
+		{
+			for (int iterator = size(); iterator > position; iterator--)
+			{
+				array[iterator] = array[iterator - 1];
+			}
+			array[position] = number;
+			counter++;
+		}
+	}
+	
+	/**
+	 * Returns the amount of values in the array.
+	 * 
+	 * @return the amount of values in the array.
+	 */
+	
+	public int size ()
+	{
+		int size = 0;
+		size = counter;
+		return size;
+	}
+	
+	/**
+	 * Returns the length the array.
+	 * 
+	 * @return the length of th array.
+	 */
+	
+	public int length ()
+	{
+		int length = 0;
+		length = array.length;
+		return length;
+	}
+	
+	/**
+	 * Deletes the value with the key from the array
+	 * while moving all values past the key's point back
+	 * and then decrements the length of the array
+	 * if it is more than 50% empty.
+	 * 
+	 * @param the key to be deleted.
+	 */
+	
+	public void delete (int key)
+	{
+		for (int iterator = 0; iterator < size(); iterator++)
+		{
+			if (array[iterator] ==  key)
+			{
+				for (int position = iterator; position < size() - 1; position++)
+				{
+					array[position] = array[position + 1];
+				}
+				array[size() - 1] = 0;
+				counter--;
+			}
+		}
+			
+		if (size() == (length() / 2))
+		{
+			int[] temp = new int[length()];
+			for (int iterator = 0; iterator < size(); iterator ++)
+			{
+				temp[iterator] = array[iterator];
+			}
+			array = new int[(length() / 2)];
+			for (int iterator = 0; iterator < array.length; iterator++)
+			{
+				array[iterator] = temp[iterator];
+			}
+		}
+	}
+	
+	/**
+	 * Prints the contents of the array in form
+	 * number space number space. Returns an empty
+	 * string if the array is empty.
+	 * 
+	 * @return the contents of the array in a string.
+	 */
+	
+	public String toString ()
+	{
+		StringBuilder line = new StringBuilder();
+		line.append("");
+		
+		for (int iterator = 0; iterator < counter; iterator++) 
+		{
+			line.append(array[iterator]);
+			if (iterator != counter - 1)
+			{
+				line.append(" ");
+			}
+		}
+		return line.toString();
+	}
+	
+	
+}
---
+++ OrderedIntListTest.java
@@ -0,0 +1,277 @@
+package cse360assign3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderedIntListTest 
+{
+	// state variables for use
+	private OrderedIntList original;
+	private OrderedIntList overLoaded;
+	int varInt;
+	
+	// makes a new OrderedIntList called original
+	// result: original will be no longer null
+	
+	@Test
+	void testOrderedIntListOriginal() 
+	{
+		original = new OrderedIntList();
+		assertNotNull(original);
+	}
+
+	// makes a new OrderedIntList called overLoaded
+	// with length 4
+	// result: overloaded will be no longer null	
+	
+	@Test
+	void testOrderedIntListWithVarInt() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		assertNotNull(overLoaded);
+	}
+	
+	// makes a new OrderedIntList called original
+	// checks size
+	// result: size will be size 0
+	
+	@Test
+	void testSizeInitial() 
+	{
+		original = new OrderedIntList();
+		int expected = 0;
+		assertEquals(expected, original.size());
+	}
+	
+	// makes a new OrderedIntList called original
+	// inserts a value, then checks size
+	// result: size will be size 1
+	
+	@Test
+	void testSizePost() 
+	{
+		original = new OrderedIntList();
+		original.insert(1);
+		int expected = 1;
+		assertEquals(expected, original.size());
+	}
+	
+	// makes a new OrderedIntList called original
+	// checks length
+	// result: length will be 10
+	
+	@Test
+	void testLengthOriginal() 
+	{
+		original = new OrderedIntList();
+		assertEquals(10,original.length());
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, checks length
+	// result: length will be 4
+	
+	@Test
+	void testLengthWithVarInt() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		assertEquals(varInt,overLoaded.length());
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 5 numbers, checks length
+	// result: length will be 6
+	
+	@Test
+	void testLengthInsertIncrement() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		overLoaded.insert(4);
+		overLoaded.insert(5);
+		int expected = 6;
+		assertEquals(expected, overLoaded.length()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 3 numbers, and deletes 1
+	// number, checks length
+	// result: length will be 2
+	
+	@Test
+	void testLengthDeleteDecrement() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);		
+		overLoaded.delete(1);
+		int expected = 2;
+		assertEquals(expected, overLoaded.length()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 1 numbers, checks contents
+	// result: contents will be "2"
+	
+	@Test
+	void testInsertIntial() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(2);
+		String expected = ("2");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 4 numbers in order, 
+	// checks contents
+	// result: contents will be "1 2 3 4"
+	
+	@Test
+	void testInsertInOrder() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		overLoaded.insert(4);
+		String expected = ("1 2 3 4");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 4 numbers out of order, 
+	// checks contents
+	// result: contents will be "1 2 3 4"
+	
+	@Test
+	void testInsertOutOrder() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(4);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		String expected = ("1 2 3 4");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 5 numbers, 
+	// checks contents
+	// result: contents will be "1 2 3 4 5"
+
+	@Test
+	void testInsertIncrementLengthContents() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(4);
+		overLoaded.insert(2);
+		overLoaded.insert(5);
+		overLoaded.insert(3);
+		String expected = ("1 2 3 4 5");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 4 numbers, deletes the
+	// first number, checks contents
+	// result: contents will be "2 3 4"
+	
+	@Test
+	void testDeleteStart() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		overLoaded.insert(4);
+		overLoaded.delete(1);
+		String expected = ("2 3 4");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 5 numbers, deletes the
+	// last number, checks contents
+	// result: contents will be "1 2 3"
+	
+	@Test
+	void testDeleteEnd() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		overLoaded.insert(4);
+		overLoaded.delete(4);
+		String expected = ("1 2 3");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 5 numbers, deletes a
+	// middle number, checks contents
+	// result: contents will be "1 2 4"
+	
+	@Test
+	void testDeleteMiddle() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);
+		overLoaded.insert(4);
+		overLoaded.delete(3);
+		String expected = ("1 2 4");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+	
+	// makes a new OrderedIntList called overLoaded
+	// with length 4, inserts 3 numbers, deletes
+	// a number, checks contents
+	// result: contents will be "2 3"
+	
+	@Test
+	void testDeleteDecrementLengthContents() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		overLoaded.insert(1);
+		overLoaded.insert(2);
+		overLoaded.insert(3);		
+		overLoaded.delete(1);
+		String expected = ("2 3");
+		assertEquals(expected, overLoaded.toString()); 
+	}
+
+	// makes a new OrderedIntList called overLoaded
+	// with length 4 checks contents
+	// result: contents will be ""
+	
+	@Test
+	void testToStringEmpty() 
+	{
+		varInt = 4;
+		overLoaded = new OrderedIntList(varInt);
+		String expected = ("");
+		assertEquals(expected, overLoaded.toString());
+	}
+
+}
